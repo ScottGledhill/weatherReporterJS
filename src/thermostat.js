c0810159a107b9370447839ca32cf4409180fbff
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-10-12 17:48:13 +0100

    decrease test passed

--- src/thermostat.js
+++ src/thermostat.js
@@ -9,6 +9,10 @@ Thermostat.prototype.increaseTemperature = function() {
   this.temperature += 1;
 };
 
+Thermostat.prototype.decreaseTemperature = function() {
+  this.temperature -= 1;
+};
+
 Thermostat.prototype.getCurrentTemperature = function() {
   return this.temperature;
 };
