26b9298baae39fc8084ad997d73dc533473519d5
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-10-12 17:16:43 +0100

    temperature can go up test added

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -10,6 +10,10 @@ describe('Thermostat', function() {
   });
 
   it('starts at 20 degrees', function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
-});
+
+  it('increases in temperature with the up button', function() {
+     thermostat.increaseTemperature();
+     expect(thermostat.getCurrentTemperature()).toEqual(21);
+   });
