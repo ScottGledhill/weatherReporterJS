05f96ac106e655cbdc55aaf2fded1e9ed28a8ff7
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-10-12 18:29:28 +0100

    test for medium usage

--- spec/usageSpec.js
+++ spec/usageSpec.js
@@ -5,7 +5,7 @@ describe('displaying usage levels', function() {
   beforeEach(function() {
     thermostat = new Thermostat();
   });
-  
+
   describe('when the temperature is below 18 degrees', function() {
     it('it is considered low-usage', function() {
       for (var i = 0; i < 3; i++) {
@@ -14,4 +14,10 @@ describe('displaying usage levels', function() {
       expect(thermostat.energyUsage()).toEqual('low-usage');
     });
   });
+
+  describe('when the temperature is between 18 and 25 degrees', function() {
+   it('it is considered medium-usage', function() {
+     expect(thermostat.energyUsage()).toEqual('medium-usage');
+   });
+ });
 });
