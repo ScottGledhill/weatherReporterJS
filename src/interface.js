9ba3bd3c872a7358a59cbb95a05834d26fc500f6
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-10-13 17:21:42 +0100

    api call for temperature added

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   thermostat = new Thermostat();
   updateTemperature();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+  });
 });
 
 $('#temp-up').on('click', function() {
