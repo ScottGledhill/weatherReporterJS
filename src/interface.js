bc4784ca7174dd2dc0a12c961fd421337e878495
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-10-14 14:50:48 +0100

    check if weather includes clouds for future display purposes

--- src/interface.js
+++ src/interface.js
@@ -37,14 +37,20 @@ $('#select-city').submit(function(event) {
 });
 
 function displayWeather(city) {
- var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
- var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
- var units = '&units=metric';
- $.get(url + token + units, function(data) {
-   $('#current-temperature').text(data.main.temp);
-   $('#wind-speed').text(data.wind.speed);
-   $('#country').text(data.sys.country);
-   $('#city').text(data.name);
-   $('#weather').text(data.weather[0].description);
- });
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+  var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+  var units = '&units=metric';
+  $.get(url + token + units, function(data) {
+    $('#current-temperature').text(data.main.temp);
+    $('#wind-speed').text(data.wind.speed);
+    $('#country').text(data.sys.country);
+    $('#city').text(data.name);
+    $('#weather').text(data.weather[0].description);
+    var weather = data.weather[0].description;
+    selectWeatherDisplay(weather);
+  });
+
+function selectWeatherDisplay(weather) {
+  console.log(weather.includes("cloud"));
+  }
 }
